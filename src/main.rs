61b500e1185640a2157b48ca0d44c4496bd04de4
kbone <kbonehobby@gmail.com>
2020-03-20 04:28:27 +0900

    Enumerate GPUs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,92 @@
+use vulkano::{
+    app_info_from_cargo_toml,
+    instance::{
+        debug::{DebugCallback, MessageSeverity, MessageType},
+        layers_list, Instance, InstanceExtensions, PhysicalDevice,
+    },
+};
+
 fn main() {
-    println!("Hello, world!");
+    println!("=== Layers ===");
+    layers_list().unwrap().for_each(|layer| {
+        println!(
+            "{} (version: {})",
+            layer.name(),
+            layer.implementation_version()
+        );
+        println!("Description: {}", layer.description());
+    });
+
+    // Create a Vulkan instance
+    let app_info = app_info_from_cargo_toml!();
+    let instance = if cfg!(debug_assertions) {
+        let extensions = InstanceExtensions {
+            ext_debug_utils: true,
+            ..InstanceExtensions::none()
+        };
+
+        // Load validation layers
+        let validation_layers = [
+            "VK_LAYER_LUNARG_standard_validation",
+            "VK_LAYER_KHRONOS_validation",
+        ];
+        let supported_validation_layers: Vec<_> = layers_list()
+            .unwrap()
+            .filter(|layer| validation_layers.contains(&layer.name()))
+            .collect();
+
+        Instance::new(
+            Some(&app_info),
+            &extensions,
+            supported_validation_layers.iter().map(|layer| layer.name()),
+        )
+    } else {
+        Instance::new(Some(&app_info), &InstanceExtensions::none(), None)
+    };
+    let instance = instance.expect("Could not build a Vulkan instance");
+
+    // Register a debugging callback
+    if cfg!(debug_assertions) {
+        let severity = MessageSeverity {
+            error: true,
+            warning: true,
+            information: true,
+            verbose: true,
+        };
+        let ty = MessageType::all();
+        DebugCallback::new(&instance, severity, ty, |message| {
+            let severity = if message.severity.error {
+                "ERROR"
+            } else if message.severity.warning {
+                "Warning"
+            } else if message.severity.information {
+                "Info"
+            } else {
+                "Verbose"
+            };
+            let ty = if message.ty.general {
+                "general"
+            } else if message.ty.validation {
+                "validation"
+            } else {
+                "performance"
+            };
+            eprintln!(
+                "{} (type: {}) (layer: {}): {}",
+                severity, ty, message.layer_prefix, message.description
+            );
+        })
+        .ok();
+    }
+
+    println!("=== Devices ===");
+    PhysicalDevice::enumerate(&instance).for_each(|device| {
+        println!("{} (index: {})", device.name(), device.index());
+        println!(
+            "API version: {}, Driver version: {}",
+            device.api_version(),
+            device.driver_version()
+        );
+        println!("Type: {:?}", device.ty());
+    });
 }
